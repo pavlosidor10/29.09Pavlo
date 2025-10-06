08fe68dc97293a3648e1b9f598941ad25db9991f
pavlosidor10 <pavlosidor10@gmail.com>
2025-10-06 17:57:34 +0300

    Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -1,48 +1,42 @@
-﻿using System;
-
 public class Enemy
 {
-    public string Name;
-    public int Level;
+    public string Name = "Розбійник";
+    public int Level = 5;
+    public int MaxHealth = 200;
     public int Health;
-    public int MaxHealth;
-    public int Attack;
-    public int Defense;
-
-    public int RewardGold;
-    public int RewardXP;
-    public string SpecialAbility;
-
-    public Enemy(string name, int level, int attack, int defense, int health, int xp, int gold, string ability)
+    public int Attack=70;
+    private int Defense=50;
+    private int RewardGold = 4000;
+    private int RewardXP=100;
+    private string SpecialAbility="None";
+    public Player player;
+    public Enemy(string name, int level, int attack, int defense, int maxhealth, int xp, int gold, string ability)
     {
         Name = name;
-        Level = level;
-        Attack = attack;
-        Defense = defense;
-        MaxHealth = health;
-        Health = health;
-        RewardXP = xp;
-        RewardGold = gold;
-        SpecialAbility = ability;
-    }
+        level = Level;
+        attack = Attack;
+        defense = Defense;
+        maxhealth = MaxHealth;
+        Health= MaxHealth;
 
+        xp = RewardXP;
+        gold = RewardGold;
+        ability = SpecialAbility;
+    }
     public static Enemy GenerateEnemy(int playerLevel)
     {
-        Random rnd = new Random();
-        int level = playerLevel + rnd.Next(-1, 2); // ±1
-        if (level < 1) level = 1;
+        return new Enemy(ability: "None", gold: 4000, xp: 500, maxhealth: 200, defense: 50, attack: 70, level: playerLevel, name: "Розбійник")
+        {
 
-        int attack = 5 + level * 2;
-        int defense = 3 + level * 2;
-        int health = 30 + level * 10;
-
-        int xp = 20 + level * 10;
-        int gold = 10 + level * 5;
-
-        string[] names = { "Гоблін" };
-        string name = names[rnd.Next(names.Length)];
-        string ability = "None"; 
-
-        return new Enemy(name, level, attack, defense, health, xp, gold, ability);
+        };
     }
+    public int CalculateAttackPower()
+    {
+        return Attack;
+    }
+    public int CalculateDefense()
+    {
+        return Defense;
+    }
+
 }

45387b6784d405a48919a142a7c5e067aec56081
pavlosidor10 <pavlosidor10@gmail.com>
2025-10-06 18:04:32 +0300

    Update Armor.cs

--- Armor.cs
+++ Armor.cs
@@ -1,10 +1,21 @@
-﻿public class Armor
+using System.Collections.Generic;
+
+public class Armor
 {
     public string Name;
     public int Price;
     public int DefenseBonus;
-
-    public override string ToString() => $"{Name} (+{DefenseBonus} захисту) - {Price} золота";
+    public static List<Armor> AllArmors = new List<Armor>();
+    public Armor() { }
+    public Armor(string name, int price, int bonus)
+    {
+        Name = name;
+        Price = price;
+        DefenseBonus = bonus;
+        AllArmors.Add(this);
+    }
+    public override string ToString()
+    {
+        return $"{Name} (+{DefenseBonus} захисту) - {Price} золота";
+    }
 }
-
-

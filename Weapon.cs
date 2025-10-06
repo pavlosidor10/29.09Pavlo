4d110a6f1aee1f3ba5e1f8d257edf3e12b358ad0
pavlosidor10 <pavlosidor10@gmail.com>
2025-10-06 18:04:59 +0300

    Update Weapon.cs

--- Weapon.cs
+++ Weapon.cs
@@ -1,8 +1,22 @@
-﻿public class Weapon
+using System.Collections.Generic;
+
+public class Weapon
 {
     public string Name;
     public int Price;
     public int AttackBonus;
+    public static List<Weapon> AllWeapons = new List<Weapon>();
+    public Weapon() { }
+    public Weapon(string name, int price, int bonus)
+    {
+        Name = name;
+        Price = price;
+        AttackBonus = bonus;
+        AllWeapons.Add(this);
+    }
 
-    public override string ToString() => $"{Name} (+{AttackBonus} атаки) - {Price} золота";
+    public override string ToString()
+    {
+        return $"{Name} (+{AttackBonus} атаки) - {Price} золота";
+    }
 }
